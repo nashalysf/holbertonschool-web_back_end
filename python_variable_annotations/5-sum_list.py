85410aa2958211bdccedaf127a9d1ce8001f7a8a
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-29 13:48:06 -0400

    sums list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Basic Python Annotations """
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns sum list of floats"""
+    return sum(input_list)
