a279a6d3c97be40c9bb64d37a2111c625145f473
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-29 13:52:11 -0400

    returns float result for mixed data types

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Basic Python Annotations """
+
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[type]) -> float:
+    """Returns sum list of floats"""
+    return sum(mxd_lst)
