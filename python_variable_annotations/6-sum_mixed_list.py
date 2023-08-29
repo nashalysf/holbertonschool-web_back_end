2ca519d14d130c3e47f09b21ce7fead915573366
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-29 14:03:04 -0400

    fixing param value

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Basic Python Annotations """
-from typing import List
 
 
-def sum_mixed_list(mxd_lst: List[type]) -> float:
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns sum list of floats"""
     return sum(mxd_lst)
