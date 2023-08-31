4cf4597a0a9287d754bf9bae30fe456388d463c2
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-31 14:54:23 -0400

    fixing typo

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,4 +12,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """ executes n times wait_random method specified by max_delay """
     tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
-
