ff00d0e7ec077c3d12a15f02295a42c6ecce3ea9
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-07 18:07:46 -0400

    gets page

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -9,12 +9,12 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ 
-    Returns tuple of size with (start-index, end-index) for pagination parameters
+    Tuple size(start, end) of pagination parameters
     """
     start_index: int = 0
     end_index: int = page * page_size
 
-    for i in range(page -1):
+    for i in range(page - 1):
         start_index += page_size
 
     return (start_index, end_index)
---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Python Pagination
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            pass
+
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """
+        Returns tuple size(start, end) for pagination parameters
+        """
+        start_index: int = 0
+        end_index: int = page * page_size
+
+        for i in range(page - 1):
+            start_index += page_size
+
+        return (start_index, end_index)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Find the correct indexes to paginate the dataset \
+            correctly and return the appropriate page of the dataset 
+
+        Args:
+            page (int, optional): _description_. Defaults to 1.
+            page_size (int, optional): _description_. Defaults to 10.
+        """
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        dataset = self.dataset()
+
+        start, end = self.index_range(page, page_size)
+        if end > len(dataset):
+            return []
+        return [list(dataset[row]) for row in range(start, end)]
