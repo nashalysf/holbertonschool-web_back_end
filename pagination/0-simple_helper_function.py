501c66bf12309cf90cb4cea2f92242e331cca0fe
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-07 17:42:22 -0400

    returns tuple of page range

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+Python Pagination
+"""
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ 
+    Returns tuple of size with (start-index, end-index) for pagination parameters
+    """
+    start_index: int = 0
+    end_index: int = page * page_size
+
+    for i in range(page -1):
+        start_index += page_size
+
+    return (start_index, end_index)
