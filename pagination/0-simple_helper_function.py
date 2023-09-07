29bb08fe465f08e91bcce7e8c1826bca0c380cf7
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-07 17:45:01 -0400

    fixing typo

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,6 +4,7 @@ Python Pagination
 """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ 
     Returns tuple of size with (start-index, end-index) for pagination parameters
