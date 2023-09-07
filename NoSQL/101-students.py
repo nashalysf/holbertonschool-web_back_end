5f72cb7f2488e8dc89da7ce50a2665b340b1d4f3
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-07 16:59:50 -0400

    returns list of students sorted by average score

---
+++ NoSQL/101-students.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+MongoDB manipulation in Python
+"""
+import pymongo
+
+
+def top_students(mongo_collection):
+    """
+    Returns students by average score
+    """
+    students = mongo_collection.aggregate({
+        "$project" : {
+            "name": "$name",
+            "averageScore": {"$agv": "$topics.score"}}
+        },
+        {
+            "$sort": {
+                "averageScore": -1
+                }
+            })
+    return students
