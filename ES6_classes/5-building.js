16e928bc8abeb1f083469cd264f0c94fa6894af8
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-15 10:04:09 -0400

    Building class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+    constructor(sqft) {
+        if (typeof(this.evacuationWarningMessage) === 'undefined' && this.constructor !== Building) throw new Error('Class extending Building must override evacuationWarningMessage');
+
+        this._sqft = sqft;
+    }
+
+    //getter sqft
+    get sqft(){
+        return this._sqft;
+    }
+
+    //setter sqft
+    set sqft(value) {
+        this._sqft = value;
+    }
+}
