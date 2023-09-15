d18873377c388a53b40b44619999ed6780c20a3a
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-15 12:04:32 -0400

    fixing code

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -15,9 +15,8 @@ export class HolbertonClass {
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
@@ -32,7 +31,7 @@ class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${self._firstName} ${self._lastName} - ${self._holbertonClass.year} - ${self._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
@@ -40,7 +39,6 @@ const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
 
 const listOfStudents = [student1, student2, student3, student4, student5];
 export default listOfStudents;
