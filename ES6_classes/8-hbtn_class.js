ead3de96e65cefd8bafeed71d91f7600211437c1
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-15 11:56:02 -0400

    fixing lint

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -9,12 +9,11 @@ export default class HolbertonClass {
 
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') {
-        return this._size;
+      return this._size;
     }
-
     if (hint === 'string') {
-        return this._location;
+      return this._location;
     }
-
+    return true;
   }
 }
