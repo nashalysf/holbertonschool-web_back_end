bc12cfbd23df78d3abb3086556c9c4fe575e6ed6
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-15 10:31:23 -0400

    fixing lint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,5 @@
 export default class HolbertonClass {
   constructor(name, length, students) {
-    if (typeof (name) !='string') throw TypeError('Name must be a string');
-    if (typeof (length) !== 'number') throw TypeError('Lenght must be a number');
-    if(!Array.isArray(students)) throw TypeError('Students must be a list');
 
     this._name = name;
     this._length = length;
@@ -26,16 +23,19 @@ export default class HolbertonClass {
 
   /* setter of name */
   set name(value) {
+    if (typeof (value) !='string') throw TypeError('Name must be a string');
     this._name = value;
   }
 
   /* setter of length */
   set length(value) {
+    if (typeof (value) !== 'number') throw TypeError('Lenght must be a number');
     this._length = value;
   }
 
   /* setter of students */
   set students(value) {
+    if(!Array.isArray(students)) throw TypeError('Students must be a list');
     this._students = value;
   }
 }
