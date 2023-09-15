1461b60810d4673e850708c65dc0f9e7493ae8d3
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-14 21:43:47 -0400

    currency constructor

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+    constructor(code, name) {
+        if (typeof(name) !== 'string') throw TypeError('Name must be a string');
+        if (typeof(code) !== 'string') throw TypeError('Code must be a string');
+
+        this._name = name;
+        this._code = code;
+    }
+    
+    //getter name
+    get name() {
+        return this._name
+    }
+
+    //getter code
+    get code() {
+        return this._code
+    }
+
+    //setter name
+    set name(value){
+        this._name = value;
+
+    }
+
+    //setter code
+    set code(value) {
+        this._code = value;
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+}
