dc901560f043dc5859579c8db7ee7d62208cf7ad
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-15 10:29:16 -0400

    fixing lint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,42 +1,41 @@
 export default class HolbertonClass {
-    constructor(name, length, students) {
-        if (typeof(name) !='string') throw TypeError('Name must be a string');
-        if (typeof(length) !== 'number') throw TypeError('Lenght must be a number');
-        if(!Array.isArray(students)) throw TypeError('Students must be a list');
+  constructor(name, length, students) {
+    if (typeof (name) !='string') throw TypeError('Name must be a string');
+    if (typeof (length) !== 'number') throw TypeError('Lenght must be a number');
+    if(!Array.isArray(students)) throw TypeError('Students must be a list');
 
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-        this._name = name,
-        this._length = length,
-        this._students = students
-    }
+  /* getter of name */
+  get name() {
+    return this._name;
+  }
 
-    //getter of name
-    get name() {
-        return this._name;
-    }
+  /* getter of length */
+  get length() {
+    return this._length;
+  }
 
-    //getter of length
-    get length() {
-        return this._length;
-    }
+  /* getter of students */
+  get students() {
+    return this._students;
+  }
 
-    //getter of students
-    get students(){
-        return this._students
-    }
+  /* setter of name */
+  set name(value) {
+    this._name = value;
+  }
 
-    //setter of name
-    set name(value) {
-        this._name = value;
-    }
+  /* setter of length */
+  set length(value) {
+    this._length = value;
+  }
 
-    //setter of length
-    set length(value) {
-        this._length = value;
-    }
-
-    //setter of students
-    set students(value){
-        this._students = value;
-    }
+  /* setter of students */
+  set students(value) {
+    this._students = value;
+  }
 }
