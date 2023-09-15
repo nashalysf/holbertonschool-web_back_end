c67ff2d78aa4e6a4613e92590f65c65ce08fb86a
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-15 10:07:26 -0400

    fixing lint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,17 @@
 export default class Building {
-    constructor(sqft) {
-        if (typeof(this.evacuationWarningMessage) === 'undefined' && this.constructor !== Building) throw new Error('Class extending Building must override evacuationWarningMessage');
+  constructor(sqft) {
+    if (typeof(this.evacuationWarningMessage) === 'undefined' && this.constructor !== Building) throw new Error('Class extending Building must override evacuationWarningMessage');
 
-        this._sqft = sqft;
-    }
+    this._sqft = sqft;
+  }
 
-    //getter sqft
-    get sqft(){
-        return this._sqft;
-    }
+    //getter sqft   
+  get sqft(){
+   return this._sqft;
+ }
 
-    //setter sqft
-    set sqft(value) {
-        this._sqft = value;
-    }
+    //setter sqft   
+  set sqft(value) {
+    this._sqft = value;
+  }
 }
