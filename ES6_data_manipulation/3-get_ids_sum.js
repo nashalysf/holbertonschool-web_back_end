d6a6b7ab49e67b52d3388a7b608de373b7f704de
Nashalys Fernandez <nashalysf@gmail.com>
2023-10-10 14:56:28 -0400

    sums students id

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(students) {
-  return students.reduce((student, sum) => sum + student.id, 0);
+  return students.reduce((sum, student) => sum + student.id, 0);
 }
