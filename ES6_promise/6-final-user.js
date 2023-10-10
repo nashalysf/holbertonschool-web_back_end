264c7d5990d79563942c1b5a5f8b196463da4dfc
Nashalys Fernandez <nashalysf@gmail.com>
2023-10-10 14:16:47 -0400

    fixing lint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,8 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName){
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((res) => {
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((res)=> {
     for (const i of res) {
       if (i.status === 'rejected') {
         i.value = `Error: ${i.reason.message}`;
