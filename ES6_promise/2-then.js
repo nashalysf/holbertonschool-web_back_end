3e4cc8ca8d783cac5137bd5b8cda642832ee77df
Nashalys Fernandez <nashalysf@gmail.com>
2023-10-10 14:06:00 -0400

    fixing lint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({
     status: 200,
-    body: 'success'
+    body: 'success',
   })).catch(() => Error()).finally(() => console.warn('Got a response from the API'));
 }
