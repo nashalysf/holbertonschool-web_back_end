322315b0193782f8ab051715b06686618dc73d8f
Nashalys Fernandez <nashalysf@gmail.com>
2023-10-10 14:15:03 -0400

    fixing typos

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName){
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((res) => {
-      for (const i of res) {
-        if (i.status === 'rejected') {
-          i.value = `Error: ${i.reason.message}`;
-          delete i.reason;
-        }
+    for (const i of res) {
+      if (i.status === 'rejected') {
+        i.value = `Error: ${i.reason.message}`;
+        delete i.reason;
       }
-      return res;
-    });
+    }
+    return res;
+  });
 }
